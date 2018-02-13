b2e8a4d09c37ccd57cbd3479bc42f331147143a9
Carlo Cruz <me@carlocruz.com>
2018-02-13 20:37:26 +1100

    Add functionality to convert to prices to candles

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { getPairPrices } from './market-utils/market';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	componentWillMount() {
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<h1>Hello</h1>
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<p className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to
+					reload.
+				</p>
+			</div>
+		);
+	}
 }
 
 export default App;
