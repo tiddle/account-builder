8404b1c0a1751a60f18086401fec62dd81621517
Carlo Cruz <me@carlocruz.com>
2018-02-17 18:13:03 +1100

    Remove unnecessary files and packages

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    // const elem = shallow(<App />);
+    // expect(elem.is('.App')).toBe(true);
 });
