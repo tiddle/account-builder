5326ecebdfad72ad6983175c5dfe22970c090f30
Carlo Cruz <me@carlocruz.com>
2018-02-21 12:41:28 +1100

    Add documentation for new function

--- src/lambda/cryptopia.js
+++ src/lambda/cryptopia.js
@@ -1,21 +1,7 @@
-import { getMarkets, getPairPrices } from '../cryptopia/market';
-import { getHighLow } from '../utils/price';
+import { getAllCandles } from '../cryptopia/market';
 
 exports.getCandles = function(aa) {
-	getMarkets()
-		.then(markets => {
-			const pp = markets
-				.slice(0, 10) // only the first 10
-				.filter(market => market.volume > 1) // only those with volumes
-				.map(market => {
-					return getPairPrices(market.id, market.volume, market.last);
-				});
-
-			Promise.all(pp).then(aa => {
-				console.log(aa);
-			});
-		})
-		.catch(err => {
-			console.log(err);
-		});
+	return getAllCandles().then(results => {
+		console.log(results);
+	});
 };
