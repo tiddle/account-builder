bb389a2585c9ea65f90be274d31a192015c46191
Carlo Cruz <me@carlocruz.com>
2018-02-22 13:08:06 +1100

    Adjust webpack to allow different outputs for different exchanges

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,35 @@
 var path = require('path');
-
-module.exports = {
-    entry: './src/lambda/binance.js',
-    output: {
-        path: path.resolve(__dirname, 'lambda-dist'),
-        filename: '[name].js',
-        library: '[name]',
-        libraryTarget: 'commonjs2'
-    },
-    target: 'node',
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015']
-            }
-        }]
-    }
+const defaultOutput = {
+	entry: './src/lambda/binance.js',
+	output: {
+		path: path.resolve(__dirname, 'lambda-dist'),
+		filename: 'binance.js',
+		library: 'index',
+		libraryTarget: 'commonjs2'
+	},
+	target: 'node',
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015']
+				}
+			}
+		]
+	}
 };
+
+const binance = Object.assign({}, defaultOutput);
+const cryptopia = Object.assign({}, defaultOutput, {
+	entry: './src/lambda/cryptopia.js',
+	output: {
+		filename: 'cryptopia.js'
+	}
+});
+
+module.exports = [binance, cryptopia];
+
+// module.exports = defaultOutput;
